1775a3144cd138872f1a87b8084b0505ad611e9f
Kasketto001 <kaskettomaledetto@gmail.com>
2024-05-13 17:31:58 +0200

    Add Faker for data insert in trains table

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
@@ -10,34 +11,54 @@ class TrainsTableSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        $trains = [
-            [
-                'company' => 'Boolean Rails',
-                'departure_station' => 'New York',
-                'arrival_station' => 'Los Angeles',
-                'departure_time' => '08:00:00',
-                'arrival_time' => '16:00:00',
-                'train_code' => 'BR123',
-                'carriages_number' => 8,
-                'on_schedule' => true,
-                'cancelled' => false,
-            ] 
-        ];
 
-        foreach ($trains as $trainData) {
-            $newTrain = new Train();
-            $newTrain->company = $trainData['company'];
-            $newTrain->departure_station = $trainData['departure_station'];
-            $newTrain->arrival_station = $trainData['arrival_station'];
-            $newTrain->departure_time = $trainData['departure_time'];
-            $newTrain->arrival_time = $trainData['arrival_time'];
-            $newTrain->train_code = $trainData['train_code'];
-            $newTrain->carriages_number = $trainData['carriages_number'];
-            $newTrain->on_schedule = $trainData['on_schedule'];
-            $newTrain->cancelled = $trainData['cancelled'];
-            $newTrain->save();
+        for($i=0; $i<20; $i++) {
+
+            $train = new Train();
+            $train->company = $faker->words(2, true);
+            $train->departure_station = $faker->word();
+            $train->arrival_station = $faker->word();
+            $train->departure_time = $faker->time();
+            $train->arrival_time = $faker->time();
+            $train->train_code = $faker->bothify('?? ###');
+            $train->carriages_number = $faker->numberBetween(8,12);
+            $train->on_schedule = $faker->boolean(90);
+            $train->cancelled = $faker->boolean(10);
+            $train->save();
         }
+        
+        
+        
+        
+        
+        // $trains = [
+        //     [
+        //         'company' => 'Boolean Rails',
+        //         'departure_station' => 'New York',
+        //         'arrival_station' => 'Los Angeles',
+        //         'departure_time' => '08:00:00',
+        //         'arrival_time' => '16:00:00',
+        //         'train_code' => 'BR123',
+        //         'carriages_number' => 8,
+        //         'on_schedule' => true,
+        //         'cancelled' => false,
+        //     ] 
+        // ];
+
+        // foreach ($trains as $trainData) {
+        //     $train = new Train();
+        //     $train->company = $trainData['company'];
+        //     $train->departure_station = $trainData['departure_station'];
+        //     $train->arrival_station = $trainData['arrival_station'];
+        //     $train->departure_time = $trainData['departure_time'];
+        //     $train->arrival_time = $trainData['arrival_time'];
+        //     $train->train_code = $trainData['train_code'];
+        //     $train->carriages_number = $trainData['carriages_number'];
+        //     $train->on_schedule = $trainData['on_schedule'];
+        //     $train->cancelled = $trainData['cancelled'];
+        //     $train->save();
+        // }
      }
 }
